f455d46c46f69d98f7bd4e694e4ffbb132389a79
flawedmatrix <flawedmatrix@gmail.com>
2012-10-13 10:29:57 -0700

    Scanner and query split

--- database.js
+++ database.js
@@ -37,10 +37,10 @@ exports.addtodb = function(datarows) {
 	connection.connect();
 	var datarow;
 	var values;
+    var k = 0;
 	console.log('arr length is ', datarows.length);
 	for(var i = 0; i < datarows.length; i++) {
-		console.log('On row ', i);
-		datarow = datarows[i];
+        datarow = datarows[i];
 		values = "('" + mysql_real_escape_string(datarow.dept) + "', '" + 
                     mysql_real_escape_string(datarow.title) + "', '" + 
                     mysql_real_escape_string(datarow.courseno) + "', '" + 
@@ -54,99 +54,11 @@ exports.addtodb = function(datarows) {
                     mysql_real_escape_string(datarow.restrictions) + "', '" + 
                     mysql_real_escape_string(datarow.note) +"')";
 		connection.query('INSERT INTO courses (dept, title, courseno, sectionno, controlno, time, room, units, instructor, examgroup, restrictions, note) VALUES ' + values, function(err, rows, fields) {
-		  if (err) throw err;
-		  console.log('Inserted course no: ', datarow.controlno);
+            if (err) throw err;
+            console.log("iteration " + k);
+            k += 1;
 		});
 	}
 	connection.end();
 	console.log('connection closed');
-};
-
-//querying
-exports.querydb = function(inputrow, callback) {
-	var connection = mysql.createConnection({
-		  host     : 'localhost',
-		  user     : 'hackathon',
-		  password : 'password',
-		  database : 'bclass',
-	});
-	connection.connect();
-	var values = '';
-	if(type(inputrow.dept) != null && inputrow.dept != undefined) {
-		values += ' dept = "'+inputrow.dept+'"';
-	}
-	if(type(inputrow.title) != null && inputrow.title != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' title = "'+inputrow.title+'"';
-	}
-	if(type(inputrow.dept) != null && inputrow.dept != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' courseno = "'+inputrow.dept+'"';
-	}
-	if(type(inputrow.sectionno) != null && inputrow.sectionno != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' sectionno = "'+inputrow.sectionno+'"';
-	}
-	if(type(inputrow.controlno) != null && inputrow.controlno != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' controlno = "'+inputrow.controlno+'"';
-	}
-	if(type(inputrow.time) != null && inputrow.time != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' time = "'+inputrow.time+'"';
-	}
-	if(type(inputrow.room) != null && inputrow.room != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' room = "'+inputrow.room+'"';
-	}
-	if(type(inputrow.units) != null && inputrow.units != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' units = "'+inputrow.units+'"';
-	}
-	if(type(inputrow.instructor) != null && inputrow.instructor != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' instructor = "'+inputrow.instructor+'"';
-	}
-	if(type(inputrow.examgroup) != null && inputrow.examgroup != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' examgroup = "'+inputrow.examgroup+'"';
-	}
-	if(type(inputrow.restrictions) != null && inputrow.restrictions != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' restrictions = "'+inputrow.restrictions+'"';
-	}
-	if(type(inputrow.note) != null && inputrow.note != undefined) {
-		if(values != '') {
-			values += ' AND';
-		}
-		values += ' note = "'+inputrow.note+'"';
-	}
-	values+=';';
-	
-	connection.query('SELECT * FROM courses WHERE'+values, function(err, rows, fields) {
-		if (err) throw err;
-		callback(rows);
-	});
-	
-	connection.end();
 };
